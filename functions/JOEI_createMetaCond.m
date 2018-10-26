ab786751fd055ed84320315b8921e396789be2aa
Daniel Matthes <dmatthes@cbs.mpg.de>
2018-10-26 17:15:29 +0200

    Function JOEI_createMetaCond – NEW function → creates a subset of trials in which a certain event (infant object look, mutual gaze, mutual object look) took place.

---
+++ functions/JOEI_createMetaCond.m
@@ -0,0 +1,55 @@
+function [ data ] = JOEI_createMetaCond( cfg, data)
+% JOEI_CREATEMETACOND creates a subset of trials in which a certain event
+% (infant object look, mutual gaze, mutual object look) took place.
+%
+% Use as
+%   [ data ] = JOEI_createMetaCond( cfg, data )
+%
+% where the input data have to be the result from either JOEI_IMPORTATASET
+% or JOEI_PREPROCESSING
+%
+% The configuration options are
+%   cfg.event     = name of event, options: 'infObj', 'mGaze', 'mObj' (default: 'infObj')
+%   cfg.eventSpec = event specification, output of JOEI_IMPORTATASET
+%                   (see file JOEI_pxx_01b_events_yyy.mat)
+%
+% This function requires the fieldtrip toolbox.
+%
+% See also JOEI_SEGMENTATION, JOEI_MANARTIFACT and JOEI_AUTOARTIFACT 
+
+% Copyright (C) 2018, Daniel Matthes, MPI CBS
+
+% -------------------------------------------------------------------------
+% Get config options
+% -------------------------------------------------------------------------
+event     = ft_getopt(cfg, 'event', 'infObj');
+eventSpec = ft_getopt(cfg, 'eventSpec', []);
+                
+if isempty(eventSpec)
+  error('cfg.eventSpec has to be defined');
+end
+
+switch event
+  case 'infObj'
+    trl = eventSpec.gaze_inf.object.trl;
+    markerOffset = 300;
+  case 'mGaze'
+    trl = eventSpec.analysis_gaze.MutualGaze.trl;
+    markerOffset = 400;
+  case 'mObject'
+    trl = eventSpec.analysis_gaze.MutualObject.trl;
+    markerOffset = 500;
+end
+
+% -------------------------------------------------------------------------
+% Create data subset
+% -------------------------------------------------------------------------
+cfg     = [];
+cfg.trl = trl;
+
+fprintf(['Create data of meta conditions which are related to the '...
+          'event: %s...\n'], event);
+data = ft_definetrial(cfg, data);
+data.trialinfo = data.trialinfo + markerOffset;
+
+end
